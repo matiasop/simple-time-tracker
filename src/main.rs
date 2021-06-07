1dbb6725932a34d7f2dd1ccd33ad13271d2e7418
movalle4 <movalle4@uc.cl>
2021-06-07 11:50:27 -0400

    add json add function

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ const MINUTES_STEP: i64 = 1;
 
 fn main() {
     // json_to_csv("days.json", "days.csv");
+    // add_json("data/j1.json", "data/j2.json", "data/result.json");
     let step = Duration::new(60 * (MINUTES_STEP as u64), 0);
 
     // Get paths
@@ -64,6 +65,7 @@ fn read_file(path: &str) -> Result<Map<String, Value>, Box<dyn Error>> {
     Ok(obj)
 }
 
+#[allow(dead_code)]
 fn json_to_csv(path_json: &str, path_csv: &str) {
     // Converts path_json to csv
 
@@ -77,4 +79,41 @@ fn json_to_csv(path_json: &str, path_csv: &str) {
         wtr.write_record(&[item.0, &item.1.as_i64().unwrap().to_string()])
             .unwrap();
     }
+}
+
+#[allow(dead_code)]
+fn add_json(first_json_path: &str, second_json_path: &str, result_path: &str) {
+    // Creates a new json with the sum of the values of first and second json
+
+    let first_parsed: Value =
+        serde_json::from_str(&fs::read_to_string(first_json_path).unwrap()).unwrap();
+    let first: Map<String, Value> = first_parsed.as_object().unwrap().clone();
+
+    let second_parsed: Value =
+        serde_json::from_str(&fs::read_to_string(second_json_path).unwrap()).unwrap();
+    let mut second: Map<String, Value> = second_parsed.as_object().unwrap().clone();
+
+    let mut result: Map<String, Value> = Map::new();
+
+    // Add every item in first to second and put it in result
+    for item in first.iter() {
+        let key: &String = item.0;
+        let val: i64 = item.1.as_i64().unwrap();
+        if second.contains_key(key) {
+            let other_val = second.get(key).unwrap().as_i64().unwrap();
+            result.insert(key.to_string(), json!(val + other_val));
+            second.remove(key);
+        } else {
+            result.insert(key.to_string(), json!(val));
+        }
+    }
+
+    // Add all remaining items in second to result
+    for item in second.iter() {
+        let key: &String = item.0;
+        let val: i64 = item.1.as_i64().unwrap();
+        result.insert(key.to_string(), json!(val));
+    }
+
+    fs::write(result_path, to_string_pretty(&result).unwrap()).expect("Unable to write to file");
 }
